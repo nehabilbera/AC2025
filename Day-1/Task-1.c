31e51ec53ad25c6e26203f8884d96486518d91d3
nehabilbera <121599292+nehabilbera@users.noreply.github.com>
2023-01-01 22:10:06 +0530

    Create Task-1.c
    Task-1 completed

---
+++ Day-1/Task-1.c
@@ -0,0 +1,25 @@
+/*
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=n-1;j>i;j--){
+            printf(" ");
+        }
+        for(j=0;j<2*i+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
