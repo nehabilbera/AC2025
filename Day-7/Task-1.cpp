8cf36f6eeb9cfdd92636470dc1ee130b1d82293d
Neha Gupta <121599292+nehabilbera@users.noreply.github.com>
2023-01-07 00:48:34 +0530

    Create Task-1.cpp
    Task-1 completed !!!!

---
+++ Day-7/Task-1.cpp
@@ -0,0 +1,57 @@
+//Searching an element in a sorted array
+
+//Time Complexity : O(N)
+//Space Complexity : O(1)
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+        int i,count=0;
+        for(i=0;i<N;i++){
+            if(arr[i]==K)
+                count++;
+        }
+        if(count==0)
+            return -1;
+        return 1;    
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
