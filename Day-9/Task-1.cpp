34ea653da34c88122c1aaa756d2ac621bedf33bb
Neha Gupta <121599292+nehabilbera@users.noreply.github.com>
2023-01-10 19:30:39 +0530

    Create Task-1.cpp
    Task-1 completed !!!!

---
+++ Day-9/Task-1.cpp
@@ -0,0 +1,65 @@
+// Power of 2.
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        while(n>1){
+            if(n%2==0){
+                n=n/2;
+                if(n==1){
+                    return true;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        if(n==1)
+        return true;
+        else
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
