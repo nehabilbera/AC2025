66f24a16abf7617c2655e9c9a634a924fd2ec220
Neha Gupta <121599292+nehabilbera@users.noreply.github.com>
2023-02-08 15:53:10 +0530

    Create Task-2.cpp
    Task-2 completed !!!

---
+++ Day-39/Task-2.cpp
@@ -0,0 +1,29 @@
+ // Find Minimum in Rotated Sorted Array
+// Time Complexity : O(logN)
+
+int pivot(vector<int>& arr , int n){
+    int low=0;
+    int high=n-1;
+    int mid=low+(high-low)/2;
+
+    while(low < high){
+        if(arr[mid] >= arr[0]){
+            low=mid+1;
+        }
+        else{
+            high=mid;
+        }
+        mid=low+(high-low)/2;
+    }
+    return low;
+}
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        if(nums[0] <= nums[n-1]) return nums[0];
+        int ans=pivot(nums,n);
+        return nums[ans];
+    }
+};
